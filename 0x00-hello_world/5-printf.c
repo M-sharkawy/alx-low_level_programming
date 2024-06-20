73b0b7a3d6fb24211fd4eb3e8eac04d700a6ff50
sharkawy <mostafasharks021@gmail.com>
2024-06-20 09:36:12 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: A C program used to print with printf
+ *
+ *  return: 0 always success
+ *
+**/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

68bce9b12b2b3b613cee66b09286fa70309f8ef8
sharkawy <mostafasharks021@gmail.com>
2024-06-21 19:00:17 -0700

    print number file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print digits from 0 to 9
+ *
+ * Return: 0 (Sucess)
+*/
+
+int main(void)
+{
+	char n = '0';
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(\n);
+
+	return (0);
+

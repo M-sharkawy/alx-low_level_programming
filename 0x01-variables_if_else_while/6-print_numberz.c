2e3e6e8ed1bb0985b4256c024c2c95963bebe546
sharkawy <mostafasharks021@gmail.com>
2024-06-21 19:17:38 -0700

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print digits from 0 to 9 using putchar
+ *
+ * Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchart(n + 48);
+			n++;
+	}
+	printf("\n");
+	return (0);
+}

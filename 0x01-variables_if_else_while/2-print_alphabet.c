5e9e906c551bbfec13e9b325254325ce27c912db
sharkawy <mostafasharks021@gmail.com>
2024-06-21 18:17:47 -0700

    point alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Entery point
+ *
+ * Description: alphabetical order
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{	char n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}

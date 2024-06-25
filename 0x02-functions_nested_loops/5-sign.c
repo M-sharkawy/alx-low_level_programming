8c3b5f30bb1c0a35a612395d874e015b7585387c
M-sharkawy <mostafasharks021@gmail.com>
2024-06-26 01:53:50 +0200

    alphabetics

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign- print signs under if conditions
+ *
+ * @n: takes integer type input for function.
+ *
+ * Return: 1 if n = + or 0 if n = 0 or -1 if n = -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(1);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}

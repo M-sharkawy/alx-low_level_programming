9393ac9bb1ec69d7ac7bbfbf87fda3c733bf8440
sharkawy <mostafasharks021@gmail.com>
2024-06-25 14:31:02 -0700

    alphabeticsx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-    int c = 'a';
-    int l = 0;
+	int c = 'a';
+	int l = 0;
 
-    for (l = 0; l <= 10 ; l++)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-            _putchar('c');
-        _putchar('\n');
-    }        
+	for (l = 0; l <= 10 ; l++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar('c');
+		_putchar('\n');
+	}
 }

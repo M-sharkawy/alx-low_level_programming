1821970bd5f90f838272fbdcadfc948b5d554119
Mostafa El-Sharkawy <157389819+M-sharkawy@users.noreply.github.com>
2024-06-25 21:25:28 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-    char s[] = "_putchar";
-    int c;
-
-    for (c = 0; c < 8; c++)
-        _putchar(s[c]);
-
-    _putchar('\n');
-    return (0);
+        char s[] = "_putchar";
+        int c;
+        
+        for (c = 0; c < 8; c++)
+                _putchar(s[c]);
+        
+        _putchar('\n');
+        return (0);
 }

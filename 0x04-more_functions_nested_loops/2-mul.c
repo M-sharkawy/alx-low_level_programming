29e38aea085d1702652f099c71d05ae2d0f14ae3
sharkawy <mostafasharks021@gmail.com>
2024-06-27 14:54:10 -0700

    first 4 files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * mul - multiple two integers
+ *
+ * a@ first input
+ * b@ second input
+ *
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+	mult = a * b
+		return (mult);
+}

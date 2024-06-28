0601deb728a940a016611c4e0a989f2a2b1f6c02
M-sharkawy <mostafasharks021@gmail.com>
2024-06-28 03:03:23 +0200

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,10 +2,7 @@
 
 /**
  * print_diagonal - draws a diagonal line on the terminal.
- * You can only use _putchar function to print
- * Where n is the number of times the character \ should be printed
- * The diagonal should end with a \n
- * If n is 0 or less, the function should only print a \n
+ *
  * @n: nthe number of times the character
  * should be printed
 */

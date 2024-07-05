acaa1e767fc765e6bcca7332f6e27d2b347c5ed0
M-sharkawy <mostafasharks021@gmail.com>
2024-07-05 03:50:08 +0200

    rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,10 +12,13 @@ void reverse_array(int *a, int n)
 
 	/* Loop through half of the array elements */
 	/* Swap elements from start and end towards the middle */
-	for (i= n - 1; i >= n / 2; i--)
+	for (i = n - 1; i >= n / 2; i--)
 	{
-		tmp = a[n - 1 - i];     /* Store the value of the element at the opposite end */
-		a[n - 1 - i] = a[i];  /* Replace the element at the opposite end with current element */
-		a[i] = tmp;  /* Replace the current element with the element from opposite end */
+		/* Store the value of the element at the opposite end */
+		tmp = a[n - 1 - i];
+		/* Replace the element at the opposite end with current element */
+		a[n - 1 - i] = a[i];
+		/* Replace the current element with the element from opposite end */
+		a[i] = tmp;
 	}
 }

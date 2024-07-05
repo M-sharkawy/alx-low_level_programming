1d8e5793dd13e9495904c6fd06232797dd6b4674
M-sharkawy <mostafasharks021@gmail.com>
2024-07-05 03:32:01 +0200

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,16 +14,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
-
-	while (*s1)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			equal = ((int)*s1 - 48) - ((int) *s2 - 48);
-		}
 		s1++;
 		s2++;
 	}
-	return (equal);
+
+	return (*s1 - *s2);
+}
 }

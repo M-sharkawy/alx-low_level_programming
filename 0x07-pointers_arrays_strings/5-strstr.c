5a9ef7a2cee3d38cea5cc58a3c7463b0988e2427
M-sharkawy <mostafasharks021@gmail.com>
2024-07-09 04:07:36 +0200

    print chess

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+
+/**
+ * _strstr - a function that locates a substring
+ *
+ * @haystack: main string to be scanned.
+ * @needle: subtring to be searched for in haystack string.
+ *
+ * Return: a pointer to the beginning
+ * of the located substring or
+ * NULL if substring is not found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{

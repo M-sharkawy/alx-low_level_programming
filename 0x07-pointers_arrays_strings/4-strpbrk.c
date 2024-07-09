914094a087bd338c6483c2e5da14faab5e49bff3
M-sharkawy <mostafasharks021@gmail.com>
2024-07-09 02:53:38 +0200

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of characters(bytes)
+ *
+ * @s: string to be scanned.
+ * @accept: string containing the characters to match.
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int index, jndex;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		for (jndex = 0; accept[jndex] != '\0'; jndex++)
+		{
+			if (s[index] == accept[jndex])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (NULL);
+}

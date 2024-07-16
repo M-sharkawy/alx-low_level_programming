b7640d704f8b1a954948248e8c89a1f440ad6d7b
M-sharkawy <mostafasharks021@gmail.com>
2024-07-16 19:16:24 +0300

    strings conconates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,46 +3,50 @@
 #include <stdlib.h>
 
 /**
- * 
- * 
+ * str_concat - concatonate 2 strings in new memory location
+ *
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: pointer for 2 concatonated strings.
 */
 
 char *str_concat(char *s1, char *s2)
 {
-    int n1 , n2, lenght;
-    int i;
-    char *ptr;
+	int n1, n2, lenght;
+	int i;
+	char *ptr;
 
-    if (s1 == NULL) 
-    {
-        s1 = "\0";
-    }
-    if (s2 == NULL) 
-    {
-        s2 = "\0";
-    }
+	if (s1 == NULL)
+	{
+		s1 = "\0";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
 
-    n1 = strlen(s1);
-    n2 = strlen(s2);
-    lenght = n1 + n2 + 1;
+	n1 = strlen(s1);
+	n2 = strlen(s2);
+	lenght = n1 + n2 + 1;
 
-    ptr = (char *)malloc(lenght * sizeof(char));
+	ptr = (char *)malloc(lenght * sizeof(char));
 
-    if (ptr == NULL)
-    {
-    return (NULL);
-    }
+	if (ptr == NULL)
+	{
+	return (NULL);
+	}
 
-    for (i = 0; i <= n1; i++)
-    {
-        ptr[i] = s1[i];
-    }
-    for (i = 0; i <= n2; i++)
-    {
-        ptr[n1 + i] = s2[i];
-    }
-    
-    ptr[n1 + n2] = '\0';
+	for (i = 0; i <= n1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (i = 0; i <= n2; i++)
+	{
+		ptr[n1 + i] = s2[i];
+	}
 
-    return (ptr);
+	ptr[n1 + n2] = '\0';
+
+	return (ptr);
 }

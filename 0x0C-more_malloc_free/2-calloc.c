90b7d9d279dfd8f4bc03db7857ddb64492b43c37
M-sharkawy <mostafasharks021@gmail.com>
2024-07-19 16:51:42 +0300

    calloc.c file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * 
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else 
+	{    
+		for (i = 0; i < nmemb * size; i++)
+			ptr[i] = 0;
+	}
+	return (ptr);
+}

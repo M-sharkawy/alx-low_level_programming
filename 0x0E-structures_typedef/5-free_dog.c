0321586b3aa948d73a847246172d32b29698eb66
M-sharkawy <mostafasharks021@gmail.com>
2024-07-23 02:37:47 +0300

    free dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
   * free_dog - frees memory allocated by dog
@@ -13,8 +13,8 @@ void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d->owner);
 		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }

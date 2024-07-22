8c5b96f8090ba1f5e3d9dc3c8602765ee7cc551e
M-sharkawy <mostafasharks021@gmail.com>
2024-07-22 19:24:21 +0300

    dog inf

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+C - Pstructures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog information
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * return: struct data type
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

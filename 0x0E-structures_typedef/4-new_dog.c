abb3e30e10887dcca0015975bbcf56cb5f38886d
M-sharkawy <mostafasharks021@gmail.com>
2024-07-23 02:54:18 +0300

    modification in dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/** 
+ * _strdup - duplicate the string in the new memory
+ *
+ * @str: the string given
+ *
+ * Return: NULL if str is NULL or a point to the duplicated string
+*/
+
+char *_strdup(char *str)
+{
+	int sorc_index;
+	int destin_index;
+	char *copy;
+
+	sorc_index = 0;
+	destin_index = 0;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[sorc_index] != '\0')
+	{
+		sorc_index = sorc_index + 1;
+	}
+
+	copy = (char *)malloc(sizeof(char) * (sorc_index + 1));
+
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+
+	while (destin_index < sorc_index)
+	{
+		copy[destin_index] = str[destin_index];
+		destin_index = destin_index + 1;
+	}
+	copy[destin_index] = '\0';
+
+	return (copy);
+}
+
 /**
  * new_dog - Creates a new dog file
  *
@@ -22,11 +65,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
   	new_dog = malloc(sizeof(dog_t));
-  	if (!new_dog) 
+  	if (!new_dog)
+	 
 	return NULL;
 
-  	new_dog->name = strdup(name);
-  	new_dog->owner = strdup(owner);
+  	new_dog->name = _strdup(name);
+  	new_dog->owner = _strdup(owner);
   	if (!new_dog->name || !new_dog->owner) 
   	{
 	free(new_dog);

7c0de7244d983bb538b3002360195f4d998a9848
M-sharkawy <mostafasharks021@gmail.com>
2024-07-25 00:47:50 +0300

    array function iteration

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,22 +3,28 @@
 #include <stdlib.h>
 #include "stddef.h"
 /**
- * 
+ * array_iterator - iterate array by execution of a function
+ *
+ * @array: pointer to first element in an array
+ * @size: size of array elements
+ * @action: pointer of function that print
+ *
+ * return: Null
 */
 
 
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL || size == 0 || action == NULL)
-    {
-        return;
-    }
-    else
-    {
-        for (i = 0; i < size; i++)
-            action(array[i]);
-    }
+	if (array == NULL || size == 0 || action == NULL)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }

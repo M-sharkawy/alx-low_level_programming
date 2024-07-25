972886b8b4db9e8b2ef51b17b9c242a38295c250
M-sharkawy <mostafasharks021@gmail.com>
2024-07-26 01:16:11 +0300

    print any no. of args with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,21 +16,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator == NULL || *separator == 0)
+	if (separator == NULL || *separator == '\0')
 	{
 		separator = ""; 
 	}
 
-	if (n > 0)
-	{
-		va_start(args, n);
-	}
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, const unsigned int));
 
-		if (i < (n - 1) && separator != NULL)
+		if (i < (n - 1))
 			printf("%s", separator);
 	}
 	va_end(args);

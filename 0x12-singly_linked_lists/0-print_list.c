75e1e070ca0b8ffaec1056f420700ede08e913e9
M-sharkawy <mostafasharks021@gmail.com>
2024-08-02 01:56:30 +0300

    print list function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+
+/**
+ * print_list - print string and length of string for each element of 'h'
+ *
+ * @h: pointer to first element of the list
+ *
+ * Return: the list lenght
+*/
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			size++;
+			continue;
+		}
+
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		size++;
+	}
+	return (size);
+}

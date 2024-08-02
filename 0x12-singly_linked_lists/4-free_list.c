60e67d32b6bd7952797503026d4027eafb95997a
M-sharkawy <mostafasharks021@gmail.com>
2024-08-02 05:26:10 +0300

    free memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees the list
+ *
+ * @head: pointer to the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		temp = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
+}

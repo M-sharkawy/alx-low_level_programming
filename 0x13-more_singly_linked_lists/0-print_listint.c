45fc03b966e105a2cfcdfdf395ab754ff364d0f7
M-sharkawy <mostafasharks021@gmail.com>
2024-08-06 22:59:10 +0300

    print list and count of nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,6 +13,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
+	if(h == NULL)
+	printf("0\n");
+
 	while (h != NULL)
 	if (h == NULL)
 	{
@@ -20,7 +23,7 @@ size_t print_listint(const listint_t *h)
 	}
 	else
 	{
-		printf("[%u]\n", h->n);
+		printf("%u\n", h->n);
 		h = h->next;
 		count++;
 	}
--- 0x13-more_singly_linked_lists/test/0-print_listint.c
+++ 0x13-more_singly_linked_lists/test/0-print_listint.c
@@ -1,21 +1,28 @@
 #include "lists.h"
+#include <string.h>
+
+/**
+ * print_listint - print string and length of string for each element of 'h'
+ *
+ * @h: pointer to first element of the list
+ *
+ * Return: the number of nodes
+*/
 
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    if (h == NULL)
-    {
-        printf("0\n");
-    }
-    else
-    {
-        while (h != NULL)
-        {
-            printf("%u\n", h->n);
-            h = h->next;
-            count++;
-        }
-    }
-    return (count);
+	while (h != NULL)
+	if (h == NULL)
+	{
+		printf("");
+	}
+	else
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }

9c386c62230ebe41e8422e5b4b09252ed1631e17
M-sharkawy <mostafasharks021@gmail.com>
2024-08-07 01:03:54 +0300

    free memory from the list and put head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,9 @@ void free_listint2(listint_t **head)
 {
     listint_t *ptr;
 
+    if (head == NULL)
+		return;
+
     while(*head)
     {
         ptr = (*head)->next;
@@ -17,5 +20,4 @@ void free_listint2(listint_t **head)
         *head = ptr;
     }
     *head = NULL;
-    ptr = NULL;
 }

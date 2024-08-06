2f68fce9504f983cff2ff6ed61d10f4609a80694
M-sharkawy <mostafasharks021@gmail.com>
2024-08-07 00:49:40 +0300

    free memory from the list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the list
+ *
+ * @head: pointer to the list
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+
+	while (head != NULL)
+	{
+		ptr = ptr->next;
+		free(head->n);
+		free(head);
+		head = ptr;
+	}
+}

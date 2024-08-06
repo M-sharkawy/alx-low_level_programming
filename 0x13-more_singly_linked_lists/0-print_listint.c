9f15d71eca87b4a8ef03a5bca3a7f7657ab68cea
M-sharkawy <mostafasharks021@gmail.com>
2024-08-06 22:52:16 +0300

    print list and count of nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,19 +11,17 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-
+	
+	while (h != NULL)
 	if (h == NULL)
 	{
 		printf("0\n");
 	}
 	else
 	{
-		while (h != NULL)
-		{
-			printf("%u\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%u\n", h->n);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }

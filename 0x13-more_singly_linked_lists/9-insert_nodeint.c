7a301eb196b3671aebd4c6b14dfae357f123e903
M-sharkawy <mostafasharks021@gmail.com>
2024-08-07 04:13:48 +0300

    insert anode

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,17 +12,16 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *new_node;
 	listint_t *ptr = *head;
-	listint_t *new_node = malloc(sizeof(listint_t));
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node || !head)
+		return (NULL);
 
 	new_node->n = n;
 	new_node->next = NULL;
-
-	if (!head || !*head)
-	{
-		return (NULL);
-	}
 
 	if (idx == 0)
 	{

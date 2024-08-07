2594190349ab990edfd160a5e81b36a5dc2cce47
M-sharkawy <mostafasharks021@gmail.com>
2024-08-07 04:09:22 +0300

    insert anode

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that inserts a new node.
+ *
+ * @head: pointer of the node pointer.
+ * @idx: index where the new node should be added.
+ * @n: data inside the node.
+ *
+ * Return: the address of the new node, or NULL if it failed
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *ptr = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!head || !*head)
+	{
+		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (i > 1)
+	{
+		ptr = ptr->next;
+		i--;
+	}
+	new_node->next = ptr->next;
+	ptr->next = new_node;
+	return (new_node);
+}

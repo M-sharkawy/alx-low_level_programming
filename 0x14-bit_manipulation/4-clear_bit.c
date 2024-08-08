ddfb8737cda0c14bf6b6fb7caff874c5cf479dbc
M-sharkawy <mostafasharks021@gmail.com>
2024-08-09 02:01:20 +0300

    return value of a bit to 0 at agiven index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,7 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned int) * 8)
 		return (-1);
 
-	*n = *n | (0 << index);
+	*n = *n & (0 << index);
 
 	return (1);
 }

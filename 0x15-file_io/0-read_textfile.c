cabc486e695feacd050c72e07b786e6dd99f0802
M-sharkawy <mostafasharks021@gmail.com>
2024-08-12 22:18:46 +0300

    read_txt_file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,15 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
 	int fd;
-	ssize_t bytes;
+	ssize_t rd;
+	ssize_t w;
 
 	if (!filename || !letters)
-	{
-		return (0);
-	}
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
 	{
 		return (0);
 	}
@@ -31,17 +26,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 	{
 		return (0);
-		close;
 	}
 
-	bytes = read(filename, buffer, letters);
-	bytes = write(STDOUT_FILENO, buffer, bytes);
-	if (bytes == -1)
+	fd = open(filename, O_RDONLY);
+		if (fd == -1)
+		{
+			return (0);
+		}
+
+	rd = read(filename, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, rd);
+	if (w == -1 || rd == -1 || w != rd)
 	{
 		return (0);
 		close(fd);
 		free(buffer);
 	}
 
-	return (bytes);
+	return (w);
 }

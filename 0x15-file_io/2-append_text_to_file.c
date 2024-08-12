3f204567d750572e361f00d277983e953c73b9db
M-sharkawy <mostafasharks021@gmail.com>
2024-08-12 23:28:59 +0300

    append text in the file with system calls

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,38 +5,38 @@
  *
  * @filename: name of the file
  * @text_content: NULL string add at the end
- * 
+ *
  * Return: 1 on success and -1 on failure
- */
+*/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, letters, w;
+	int fd, letters, w;
 
-    if (!filename)
-    {
-        return (-1);
-    }
+	if (!filename)
+	{
+		return (-1);
+	}
 
-    if (text_content == NULL)
-    {
-        return;
-    }
-    else
-    {
-        for (letters = 0; text_content[letters]; letters++)
-        ;
-    }
+	if (text_content == NULL)
+	{
+		return;
+	}
+	else
+	{
+		for (letters = 0; text_content[letters]; letters++)
+		;
+	}
 
-    fd = open(filename, O_WRONLY | O_APPEND, 0665);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND, 0665);
+	if (fd == -1)
+		return (-1);
 
-    w = write(fd, text_content, letters);
-    if (w == -1)
-        return (-1);
+	w = write(fd, text_content, letters);
+	if (w == -1)
+		return (-1);
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }

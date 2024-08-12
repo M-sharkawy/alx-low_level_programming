5b8eb2f604fd57f1916bf8a674bfe968e1ac4f22
M-sharkawy <mostafasharks021@gmail.com>
2024-08-12 23:10:54 +0300

    creat file with system calls

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, letters;
+	int fd, letters, w;
 
 	if (!filename)
 	{
@@ -25,15 +25,22 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	for (letters = 0; text_content[letters]; letters++)
-	{};
+	;
 
-	fd = open(filename, O_CREAT || O_WRONLY || O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	write(fd, text_content, letters);
+	w = write(fd, text_content, letters);
+	if (w == -1)
+	{
+        close(fd);
+        return (-1);
+    }
+
+	close(fd);
 
 	return (1);
 }

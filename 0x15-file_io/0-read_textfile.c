bf9d2bf5f7a5e2184933a3f873ffe7d9eb5bbc6e
M-sharkawy <mostafasharks021@gmail.com>
2024-08-12 22:34:44 +0300

    read_txt_file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,11 +29,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	fd = open(filename, O_RDONLY);
-		if (fd == -1)
-		{
-			free(buffer);
-			return (0);
-		}
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
 	rd = read(filename, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, rd);

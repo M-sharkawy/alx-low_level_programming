1b4fd58b26d73f479a02bfb6ac5b19b4e91d14b1
M-sharkawy <mostafasharks021@gmail.com>
2024-09-20 01:39:24 +0300

    function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,31 +12,36 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *temp = *h;
-    unsigned int count = 0;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int count = 0;
 
-    if (!h) return (NULL);
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node) return (NULL);
-    new_node->n = n;
+	if (!h)
+	return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+	return (NULL);
+	new_node->n = n;
 
-    if (idx == 0)
-    {
-        new_node->next = *h, new_node->prev = NULL;
-        if (*h) (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h)
+		(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
-    while (temp && count < idx - 1)
-        temp = temp->next, count++;
+	while (temp && count < idx - 1)
+		temp = temp->next, count++;
 
-    if (!temp || (temp->next == NULL && count < idx - 1))
-        return (free(new_node), NULL);
+	if (!temp || (temp->next == NULL && count < idx - 1))
+		return (free(new_node), NULL);
 
-    new_node->next = temp->next, new_node->prev = temp;
-    if (temp->next) temp->next->prev = new_node;
-    temp->next = new_node;
+	new_node->next = temp->next, new_node->prev = temp;
+	if (temp->next)
+	temp->next->prev = new_node;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }

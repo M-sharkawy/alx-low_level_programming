5bbe8488dbda429ac1bcf0b3704d58a0744908dc
M-sharkawy <mostafasharks021@gmail.com>
2024-10-11 23:54:29 +0300

    create add function to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,18 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	newnode->key = strdup(key);
-	if (!newnode->key)
-	{
-		free(newnode);
-		return (0);
-	}
 	newnode->value = strdup(value);
-	if (newnode->value)
-	{
-		free(newnode->key);
-		free(newnode);
-		return (0);
-	}
 	newnode->next = ht->array[index];
 	ht->array[index] = newnode;
 	return (1);
